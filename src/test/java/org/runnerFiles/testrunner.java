184fb67e8a727964abf65a0a9dc2d5bd41245a4d
chaithanya <chaithanyavallabhaneni2001@gmail.com>
2024-10-10 08:38:56 +0530

    Latest

--- src/test/java/org/runnerFiles/testrunner.java
+++ src/test/java/org/runnerFiles/testrunner.java
@@ -1,3 +1,5 @@
+package org.runnerFiles;
+
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 // src/test/java/runners/TestRunner.java
--- src/test/java/org/testNG/login.java
+++ src/test/java/org/testNG/login.java
@@ -1,90 +1,80 @@
-package org.example;
+package org.testNG;
 
-import io.cucumber.java.After;
-import io.cucumber.java.Before;
-import io.cucumber.java.en.And;
-import io.cucumber.java.en.Given;
-import io.cucumber.java.en.Then;
-import io.cucumber.java.en.When;
+
+import com.beust.jcommander.Parameter;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.pages.loginPage;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import org.pages.loginPage;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
 
 import java.time.Duration;
 
-public class FunctionalityOfLoginPage {
+public class login {
     public WebDriver driver;
     public loginPage login;
     public WebDriverWait wait;
 
-    @Before
-    public void setup(){
-        WebDriverManager.chromedriver().setup();
+    @Parameters({"br"})
+    @BeforeMethod
+    public void setup(String br ){
+        switch (br.toLowerCase()){
+            case "chrome":WebDriverManager.chromedriver().setup();driver = new ChromeDriver();break;
+            case "edge":WebDriverManager.edgedriver().setup();driver = new EdgeDriver();break;
+            default: System.out.println("Invalid browser");return;
+        }
         driver = new ChromeDriver();
         login = new loginPage(driver);
+        wait = new WebDriverWait(driver,Duration.ofSeconds(10));
+        driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-        wait = new WebDriverWait(driver,Duration.ofSeconds(5));
     }
 
-    @Given("I am on the movie app login page")
-    public void navigateToUrl(){
+    @Test(priority = 1)
+    public  void UI() {
         driver.get("https://qamoviesapp.ccbp.tech");
-    }
-
-    @When("Website logo image is displayed,Heading text is \"Login\",Username label text is \"USERNAME\",Password label text is \"PASSWORD\"")
-    public void loginPage(){
         Assert.assertTrue(login.isLogoDisplayed(),"Logo isn't display");
         Assert.assertEquals(login.isHeadTextDisplayed(),"Login");
         Assert.assertEquals(login.isUsernameLabelDisplayed(),"USERNAME");
         Assert.assertEquals(login.isPasswordLabelDisplayed(),"PASSWORD");
-
-    }
-
-    @And("\"Login\" button")
-    public void loginButton(){
         Assert.assertEquals(login.isLoginTextVisible(),"Login");
         Assert.assertEquals(login.isLoginFontFamilyVisible(),"Roboto");
         Assert.assertEquals(login.isLoginBackGroundColorVisible(),"rgba(229, 9, 20, 1)");
+
     }
 
-    @When("I enter valid username and password,I click on the login button")
-    public  void invalidPassword(){
+    @Test(priority = 2)
+    public void  successfullLogin(){
+        driver.get("https://qamoviesapp.ccbp.tech");
         Assert.assertTrue(login.areInputEmpty());
         login.login("rahul","rahul@2021");
         login.clickOnLogButton();
-    }
-
-    @Then("I should be redirected to the home page")
-    public void isRedirect(){
         wait.until(ExpectedConditions.urlToBe("https://qamoviesapp.ccbp.tech/"));
         Assert.assertEquals(driver.getCurrentUrl(),"https://qamoviesapp.ccbp.tech/");
-
-       }
-
-    @When("I enter valid username and invalid password,I click on the login button")
-    public  void validPassword(){
+    }
+    
+    @Test(priority = 3)
+    public void  unsuccessfullLogin(){
+        driver.get("https://qamoviesapp.ccbp.tech");
         Assert.assertTrue(login.areInputEmpty());
         login.login("rahul","rahul");
         login.clickOnLogButton();
-    }
-
-    @Then("Error will be  message appear")
-    public void errorMsg(){
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("error-message")));
         Assert.assertEquals(login.errorMessage(),"*username and password didn't match");
-
     }
 
-    @After
+    @AfterMethod
     public void tearDown(){
-        driver.quit();
+       if (driver != null) {
+           driver.quit();
+       }
     }
-
 }
